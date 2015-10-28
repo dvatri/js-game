23108ad43318860f1dd604018031e04f4832102a
Anatoliy Schavelev <a.e.schavelev@gmail.com>
2015-10-28 16:08:34 +0300

    Add dynamic tails to style and script file paths to avoid old caches

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<?php
+    $scriptTail = md5(filemtime(__DIR__ . DIRECTORY_SEPARATOR . 'script.js'));
+    $styleTail = md5(filemtime(__DIR__ . DIRECTORY_SEPARATOR . 'style.css'));
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <script src="jquery.min.js" type="text/javascript"></script>
-        <script src="script.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="style.css">
+        <script src="script.js?<?= $scriptTail ?>" type="text/javascript"></script>
+        <link rel="stylesheet" href="style.css?<?= $styleTail ?>">
     </head>
     <body onload="GAME.init()">
         <div id="gameField">
